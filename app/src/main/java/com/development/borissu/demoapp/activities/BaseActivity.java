c277e5192ae007bf376286eaccff1c0cd6f37c41
BorisSu <BorisSu@chailease.com.tw>
2018-07-22 12:27:30 +0800

    0722-shared NavagationView

--- app/src/main/java/com/development/borissu/demoapp/activities/BaseActivity.java
+++ app/src/main/java/com/development/borissu/demoapp/activities/BaseActivity.java
@@ -1,8 +1,13 @@
 package com.development.borissu.demoapp.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import butterknife.ButterKnife;
 
 public class BaseActivity extends AppCompatActivity {
+
+
 
 
 }
--- app/src/main/java/com/development/borissu/demoapp/activities/BaseNavigationActivity.java
+++ app/src/main/java/com/development/borissu/demoapp/activities/BaseNavigationActivity.java
@@ -1,4 +1,137 @@
 package com.development.borissu.demoapp.activities;
 
-public class BaseNavigationActivity extends BaseActivity{
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.development.borissu.demoapp.R;
+
+public class BaseNavigationActivity extends BaseActivity implements
+        DrawerLayout.DrawerListener
+        , NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout NavActivityLayout;
+    FrameLayout mFrameLayout;
+    NavigationView mNavigationView;
+
+
+    protected int currentMenuItem;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //直接取得Drawer Layout
+        NavActivityLayout = (DrawerLayout) inflater.inflate(R.layout.navigation_menu, null, false);
+
+        mFrameLayout = NavActivityLayout.findViewById(R.id.content_frame);
+        mNavigationView = NavActivityLayout.findViewById(R.id.nav_view);
+
+        NavActivityLayout.addDrawerListener(this);
+        mNavigationView.setNavigationItemSelectedListener(this);
+        //將Activity的內容放去FrameLayout內
+        inflater.inflate(layoutResID, mFrameLayout, true);
+
+        //設定ToolBar使其顯示Menu Icon
+        Toolbar toolbar = NavActivityLayout.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+
+        //將組合後的畫面set給Activity
+        super.setContentView(NavActivityLayout);
+
+    }
+
+    private void setupNavigation() {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavActivityLayout.openDrawer(GravityCompat.START);
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Drawer Listener
+     *
+     * @param drawerView
+     * @param slideOffset
+     */
+    @Override
+    public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+    }
+
+    @Override
+    public void onDrawerOpened(@NonNull View drawerView) {
+
+    }
+
+    @Override
+    public void onDrawerClosed(@NonNull View drawerView) {
+
+    }
+
+    @Override
+    public void onDrawerStateChanged(int newState) {
+
+    }
+
+    /**
+     * Navigation Item Selected Listener
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent it = new Intent();
+        switch (item.getItemId()) {
+            case R.id.nav_web_view:
+                it.setClass(this, WebViewActivity.class);
+                startActivity(it);
+                break;
+
+        }
+
+
+        return false;
+    }
+
+    //    public void setToolBar() {
+//        setSupportActionBar(mToolBar);
+//
+//    }
 }

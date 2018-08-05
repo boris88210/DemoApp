246a55f86766ed3d0040f9be2895aeb987017dfc
BorisSu <BorisSu@chailease.com.tw>
2018-08-05 10:07:19 +0800

    0730-add add some files

---
+++ app/src/main/java/com/development/borissu/demoapp/customized_widget/MyBehavior.java
@@ -0,0 +1,8 @@
+package com.development.borissu.demoapp.customized_widget;
+
+import android.support.design.widget.CoordinatorLayout;
+import android.view.View;
+
+public class MyBehavior extends CoordinatorLayout.Behavior<View> {
+
+}
